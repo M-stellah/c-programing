1cc6c0caadff9fc72a9f66d5a852e40f44bfda5d
M-stellah <91825160+M-stellah@users.noreply.github.com>
2021-11-09 16:01:31 +0300

    Add files via upload

---
+++ division.c
@@ -0,0 +1,13 @@
+/*Arithmetic operation division*/
+#include <stdio.h>
+int main()
+{
+	int x, y,division;
+    printf("enter a value to x\n: ");
+    scanf("%d", &x);
+    printf("enter a value to y\n: ");
+    scanf("%d", &y);
+    division=x/y;
+    printf("division=%d\n", division);
+    return 0;
+  }
